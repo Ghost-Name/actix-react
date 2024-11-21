2460f3374b9c2a8065f1133854d40cc4b13474e2
Ghost-Name <gureev.ilya.nvk@mail.ru>
2024-11-21 23:10:29 +0400

    Update dev

---
+++ client/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './sidebar.css'; 
+
+const Sidebar: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    return (
+        <div> Empty BIBA</div>
+    );
+};
+
+export default Sidebar;
